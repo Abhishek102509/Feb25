a10651b1372df0331a1405d5dadeb6bdaa1f4880
Abhishek yadav <abhi102509@gmail.com>
2025-02-17 20:43:26 +0530

    Initial commit

---
+++ Demo.java
@@ -0,0 +1,25 @@
+class Demo{
+public static void main(String arg[]){
+
+/*
+int a= 10;
+System.out.println("value of a="+ a);
+*/
+/*
+// byte a1=131;//incompatible type:
+byte a = (byte)-129;
+System.out.println(a);
+*/
+
+
+int a = 10; //define &init
+int a1;      //define
+
+short s = (byte)a;
+System.out.println(s);
+
+
+
+
+}
+}
