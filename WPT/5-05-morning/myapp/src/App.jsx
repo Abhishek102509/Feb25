709ca29a358c99c519fee66ce646aad0c4b2f7da
Abhishek yadav <abhi102509@gmail.com>
2025-05-13 12:17:25 +0530

    commit

---
+++ WPT/5-05-morning/myapp/src/App.jsx
@@ -0,0 +1,12 @@
+import { MyComponent } from "./MyComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <MyComponent></MyComponent>
+    </div>
+  )
+}
+
+export default App
---
+++ WPT/5-05-morning/myapp/src/MyComponent.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export class MyComponent extends Component{
+
+    render(){ // it delivers the UI for this class component
+        return (
+            <div>
+                <h1>Welcome to my component</h1>
+                
+            </div>
+        )
+    }
+}
